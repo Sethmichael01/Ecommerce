d2f8e9a74bd35b074945ef299d9682baa31fe9e7
Sethmichael01 <49845849+Sethmichael01@users.noreply.github.com>
2020-03-10 16:32:08 +0100

    Commit

---
+++ Ecommerce/Ecommerce/Models/Home/HomeIndexViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.DAL;
+using Ecommerce.Repository;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models.Home
+{
+    public class HomeIndexViewModel
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        DbmyOnlineShoppingEntities context = new DbmyOnlineShoppingEntities();
+        public IPagedList<Tbl_Product> ListOfProducts { get; set; }
+        public HomeIndexViewModel CreateModel(string search, int pageSize, int? page)
+        {
+            SqlParameter[] param = new SqlParameter[]{
+                new SqlParameter("@search",search??(object)DBNull.Value)
+            };
+            IPagedList<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch @search", param).ToList().ToPagedList(page ?? 1, pageSize);
+            return new HomeIndexViewModel
+            {
+                ListOfProducts = data
+            };
+        }
+    }
+}
